622ac863eef8a25fb9095c9a550ae1dbec1586ce
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-11 05:39:56 +0000

    step 4

--- https_example.js
+++ https_example.js
@@ -14,3 +14,9 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+} 
