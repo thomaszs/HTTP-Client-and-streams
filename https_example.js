eab6acb26d9ca9fbdad78364919dcc3e378456f6
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-11 05:49:07 +0000

    failed attempt

--- https_example.js
+++ https_example.js
@@ -1,9 +1,8 @@
 var https = require('https');
 var options = {
-  host: 'www.example.org',
-  path:'/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
-
 // called by https when the request is made.
 var callback = function() {
   console.log('In response handler callback!');
@@ -14,9 +13,12 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
-
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
-} 
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+}
